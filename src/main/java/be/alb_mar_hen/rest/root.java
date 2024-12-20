66a5c75c6643bd240d656245e03773cb1a9e17db
GaryScript <gary.albrecq@condorcet.be>
2024-12-20 20:36:54 +0100

    add root path

--- src/main/java/be/alb_mar_hen/rest/root.java
+++ src/main/java/be/alb_mar_hen/rest/root.java
@@ -1,5 +1,9 @@
 package be.alb_mar_hen.rest;
 
-public class root {
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
 
+@ApplicationPath("/api") 
+public class Root extends Application {
+	// nothing needed here, it's just the path of the app
 }
