ae79db03e0cd3e51152a660dc0d12125aceebaf3
cocoyote2 <maraindimitri@gmail.com>
2025-01-01 00:36:03 +0100

    Changed procedure for get maintenances for worker

--- src/main/java/be/alb_mar_hen/api/MaintenanceAPI.java
+++ src/main/java/be/alb_mar_hen/api/MaintenanceAPI.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -42,4 +43,24 @@ public class MaintenanceAPI {
 					.build();
 		}
     }
+	
+	@GET
+	@Path("/{workerid}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getMaintenanceByWorkerId(@PathParam("workerid") int workerId) {
+		try {
+			List<Maintenance> maintenances = maintenanceDAO.findall(workerId);
+
+			ObjectMapper objectMapper = new ObjectMapper();
+			objectMapper.registerModule(new Jdk8Module());
+
+			String jsonString = objectMapper.writeValueAsString(maintenances);
+
+			System.out.println("Json : " + jsonString);
+
+			return Response.status(Status.OK).entity(jsonString).build();
+		} catch (Exception e) {
+			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+		}
+	}
 }
